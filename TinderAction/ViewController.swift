85f0c4c8a9ed139470f7a3d9ae443babbe3ddfbc
“KEEVIN <keevin.mitchell@gmail.com>
2015-12-22 02:04:34 -0500

    Initial Commit

---
+++ TinderAction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TinderAction
+//
+//  Created by KEEVIN MITCHELL on 12/22/15.
+//  Copyright © 2015 beyond2021. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
